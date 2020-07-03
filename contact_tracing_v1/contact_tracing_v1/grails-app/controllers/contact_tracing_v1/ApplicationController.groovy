aa4b30d7895aa0349d5e2ce202a3a04ff00fdedd
michael <michaellorenz.labrador08@gmail.com>
2020-07-04 02:47:57 +0800

    trial

---
+++ contact_tracing_v1/contact_tracing_v1/grails-app/controllers/contact_tracing_v1/ApplicationController.groovy
@@ -0,0 +1,14 @@
+package contact_tracing_v1
+
+import grails.core.GrailsApplication
+import grails.plugins.*
+
+class ApplicationController implements PluginManagerAware {
+
+    GrailsApplication grailsApplication
+    GrailsPluginManager pluginManager
+
+    def index() {
+        [grailsApplication: grailsApplication, pluginManager: pluginManager]
+    }
+}
---
+++ contact_tracing_v1/contact_tracing_v1/grails-app/init/contact_tracing_v1/BootStrap.groovy
@@ -0,0 +1,9 @@
+package contact_tracing_v1
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
